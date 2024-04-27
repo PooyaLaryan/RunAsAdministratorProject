e6ec7ede0cf7663430f90472594d3a7fd769a74e
p.lariyan <pooya.laryan@gmail.com>
2024-04-27 18:28:51 +0330

    Add project files.

---
+++ RunAsAdminProject/Program.cs
@@ -0,0 +1,16 @@
+﻿//using System.Security.Principal;
+
+/*
+ 1 - Add application manifest file to project
+ 2 - Change application setting to "app.manifest"
+ 3 - Update tag of "requestedExecutionLevel" to requireAdministrator.
+ */
+
+
+// not needed
+/*WindowsIdentity user = WindowsIdentity.GetCurrent();
+WindowsPrincipal principal = new WindowsPrincipal(user);
+var isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);*/
+
+
+Console.ReadLine();
